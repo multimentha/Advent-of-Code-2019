bee70420c0ab1b916cb4d7066629f5d0b8b007ed
multimentha <windfall@posteo.de>
2019-12-14 19:22:48 +0100

    Illustrate asteroid field traversal with text

--- 10 - Monitoring Station/monitoring_station.py
+++ 10 - Monitoring Station/monitoring_station.py
@@ -11,9 +11,30 @@ class AsteroidField():
         self.location_map = [[c for c in line] for line in text_representation.splitlines()]
         self.width = len(self.location_map[0])
         self.height = len(self.location_map)
+        self.tile_count = self.width * self.height
     
     def visualize(self):
         return self.text_representation
+
+    def illustrate_spiral_path(self, start_x: int, start_y: int) -> str:
+        """Illustrate the path of the search spiral for a given starting position is taking.
+
+        Returns a string with each line representing a row of the grid. The numbers grow from zero and indicate the path of traversal."""
+        # represent path in 2d grid
+        grid = [[None for tile in range(self.width)] for line in range(self.height)]
+        center = (start_x, start_y)
+        spiral_path = self.spiral_path(*center)
+        spiral_path.insert(0, center)
+        hop = 0
+        digits = len(str(self.tile_count))
+        for tile in spiral_path:
+            x, y = tile[0], tile[1]
+            grid[y][x] = str(hop).rjust(digits).center(digits+2)
+            hop += 1
+        # convert list grid to text lines
+        lines = ["".join(str(num) for num in row) for row in grid]
+        grid_representation = "\n".join(lines)
+        return grid_representation
 
     def spiral_path(self, start_x: int, start_y) -> list:
         """Yield (x,y) coordinates of a spiraled path around the start position (0 based).
@@ -31,16 +52,16 @@ class AsteroidField():
         # assemble path tile by tile
         path = []
         tiles_to_go = self.width * self.height
-        edge_length = 3; next_turn_in = edge_length
+        edge_length = 2; next_turn_in = edge_length
         directions = (
             (+1, 0), # right
             (0, +1), # down"
             (-1, 0), # left"
             (0, -1), # up"
         )
-        rounds = 0 # debug stat: how often the path has spiraled around the center
+        rings = 0 # debug stat: how often the path has spiraled around the center
         direction = 0 # index for the above tuple
-        tile = (start_x - 1, start_y -1)
+        tile = (start_x - 1, start_y - 1)
         while tiles_to_go:
             if tile_inside_grid(*tile):
                 path.append(tile)
@@ -51,8 +72,10 @@ class AsteroidField():
             if next_turn_in == 0:
                 direction += 1
                 if direction == len(directions):
-                    rounds += 1
+                    rings += 1
                     edge_length += 2
                     direction = 0
+                    # offset position by 1 tile diagonally towards top left
+                    tile[0] -= 1; tile[1] -= 1
                 next_turn_in = edge_length
         return path
--- 10 - Monitoring Station/test_monitoring_station.py
+++ 10 - Monitoring Station/test_monitoring_station.py
@@ -9,9 +9,24 @@ for af in afs:
     print("==== visualisation ====")
     print(af.visualize(), "\n")
     print("==== location map ====")
-    pp.pprint(af.location_map, "\n")
+    pp.pprint(af.location_map)
+    print()
 
     # visualization of spiral path
     print("==== spiral path ====")
     grid = [[None for tile in range(af.width)] for line in range(af.height)]
-    print(grid, "\n")
+    center = (2, 2)
+    spiral_path = af.spiral_path(*center)
+    spiral_path.insert(0, center)
+    hop = 0
+    for tile in spiral_path:
+        x, y = tile[0], tile[1]
+        grid[y][x] = str(hop)
+        hop += 1
+    pp.pprint(grid)
+    print()
+
+    print("==== path illustration ====")
+    path_illustration = af.illustrate_spiral_path(2, 2)
+    print(path_illustration)
+
